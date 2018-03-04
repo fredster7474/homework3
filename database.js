b960db8e4cdbe48c07433df6e49cc17bf23b664b
Freddie Simons <freddie_simons@hotmail.co.uk>
2018-03-04 13:33:34 +0100

    adds databse fle

---
+++ database.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  'postgres',
+  'postgres',
+  'secret',
+  {
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+module.exports = sequelize
--- seeders/20180304115625-events.js
+++ seeders/20180304115625-events.js
@@ -1,16 +1,15 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    const products = [
-      { name: 'Knife', price: 10, description: 'Very nice if you want to eat something', image: null },
-      { name: 'Fork', price: 15, description: 'Very nice to put something in your mouth', image: 'http://country929.com/files/2015/04/fork.jpg' },
-      { name: 'Spoon', price: 21, description: 'If you like soup, this thing is what you need', image: 'http://i.huffpost.com/gen/1302381/images/o-SPOON-facebook.jpg' }
-    ].map(product => ({ ...product, createdAt: 'NOW()', updatedAt: 'NOW()' }))
-    return queryInterface.bulkInsert('products', products, {});
-  },
+ up: (queryInterface, Sequelize) => {
+   return queryInterface.bulkInsert('Events', [
+     { title: 'Gym', startDate: '2018-10-08 07:00:00', endDate: '2018-10-08 00:00', description: 'Feel the burn!' },
+     { title: 'Dublin', startDate: '2018-01-12 09:30:00', endDate: '2018-02-01 18:00', description: 'Holiday' }
+     { title: 'Paris', startDate: '2018-10-12 09:30:00', endDate: '2018-10-14 18:00', description: 'Holiday2' }
+   ])
+ },
 
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('products', null, {});
-  }
+ down: (queryInterface, Sequelize) => {
+   return queryInterface.bulkDelete('Events', null, {});
+ }
 };
